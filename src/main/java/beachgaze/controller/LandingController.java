20e6aa51d8210a3c08b13045f9a049149c94af2a
Zoe <zuoy1563@gmail.com>
2018-03-29 00:39:41 +1100

    delete useless models

--- src/main/java/beachgaze/controller/LandingController.java
+++ src/main/java/beachgaze/controller/LandingController.java
@@ -3,38 +3,36 @@ package beachgaze.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
-
 @Controller
 public class LandingController {
 
     @RequestMapping("/")
-    public String landing(Map<String, Object> model) {
+    public String landing() {
         return "home/index";
     }
 
     @RequestMapping("/services")
-    public String services(Map<String, Object> model) {
+    public String services() {
         return "home/services";
     }
 
     @RequestMapping("/about")
-    public String about(Map<String, Object> model) {
+    public String about() {
         return "home/about";
     }
 
     @RequestMapping("/statistics")
-    public String statistics(Map<String, Object> model) {
+    public String statistics() {
         return "home/statistics";
     }
 
     @RequestMapping("/tips")
-    public String tips(Map<String, Object> model) {
+    public String tips() {
         return "home/tips";
     }
 
     @RequestMapping("/lifeguards")
-    public String lifeguards(Map<String, Object> model) {
+    public String lifeguards() {
         return "home/lifeguards";
     }
 
